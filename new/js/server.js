115335e9e337c07018b3206806bffd89af8086fb
unknown <nluengo@nb-nluengo.equifax.cl>
2014-06-02 20:47:05 -0400

    adding new changes

--- new/js/server.js
+++ new/js/server.js
@@ -2,8 +2,9 @@ var app = require('express.io')()
 app.http().io()
 app.listen(80);
 
-var maxPlayers = 3;
-var players = [new Player(0,0,0),new Player(30,0,1),new Player(0,30,2)];
+var maxPlayers = 4;
+//var players = [new Player(0,0,0),new Player(30,0,1),new Player(0,30,2),new Player(30,30,3)];
+var players = {};
 var index = 0;
 var fullOfPlayers = -1;
 
@@ -11,9 +12,13 @@ var indexPage = '/index.html'
 var noSlot = '/noSlot.html'
 var client = '/pacman-noobteam.html'
 var gamePage = '/pacman-noobteam.html'
+var welcome = '/welcome.html'
+
+
+
+
 
 app.get('/', function (req, res) {
-
   if(index > maxPlayers){
 	res.sendfile(__dirname + noSlot);
   }else { 
@@ -21,69 +26,41 @@ app.get('/', function (req, res) {
 
   }
 });
-
-app.get('/client', function (req, res) {
-  res.sendfile(__dirname + client);
- });
  
 app.io.sockets.on('connection', function (socket) {
   console.log('Someone has connected \n');
   console.log('Number of players: '+index+ '\n');
-  socket.on('my other event', function (data) {
-    console.log(data);
-	socket.broadcast.emit('doSomething');
-  });
   
   socket.on('requestID',function (data) {
 	console.log('server - request ID received');
     if(index < maxPlayers){
+		players[index] = new Player(0,0,index);
+		console.log(players[index]);
 		socket.emit('requestedID',index);
-		console.log('Request ID required');
+		//TODO change players[index] for the GameObject
+		//app.io.sockets.emit('stageState',players[index]);
 		index++;
+		console.log('Request ID required');
+		
 	}else{
 		socket.emit('requestedID',fullOfPlayers);
 	}
   });
   
-  socket.on('sendNewPlayer',function (newPlayer) {
-	players[newPlayer.id] = newPlayer;
-	console.log('server - receiving new Player' + JSON.stringify(newPlayer));
-	console.log('server - updated list of players' + JSON.stringify(players));
-    var playersAsJSON = {"players":
-        players
-	};
-   // socket.broadcast.emit('newPlayerOnGame',playersAsJSON);
-      app.io.sockets.emit('newPlayerOnGame',playersAsJSON);
-  });
-  
-  socket.on('moveClientToServer', function(jsonAsString) {
-	var parsedJSON = JSON.parse(jsonAsString);
-	var id = parsedJSON.id;
-	
-	//actualizando posicion del player
-	players[id].xPos = parsedJSON.posx;
-	players[id].yPos = parsedJSON.posy;
-	
-	var playersAsJSON = {"players":
-        players
-	};
-	
-	//Reenviando position JSON del player a todos los demas
-	//console.log('server - init Broadcasting ' + JSON.stringify(playersAsJSON));
-	//socket.broadcast.emit('moveServerToClient',playersAsJSON);
-      app.io.sockets.emit('moveServerToClient',playersAsJSON);
-	
-  });
-  
 });
+
+
 
 function Player(x,y,playerId){
 	this.id = playerId;
 	this.xPos = x;
 	this.yPos = y;
 	this.color="#000000";
+	this.action="";
 	this.actionUp=false;
 	this.actionDown=false;
 	this.actionLeft=false;
 	this.actionRigth=false;
+	this.points=0;
 }
+
