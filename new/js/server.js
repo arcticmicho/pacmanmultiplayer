da85d938f4af8accd0ddad290ded9a184c8f79c3
gdeioannes <gdeioannes@gmail.com>
2014-06-19 21:17:40 -0400

    Fixed server

--- new/js/server.js
+++ new/js/server.js
@@ -1,101 +1,3 @@
-var app = require('express.io')()
-app.http().io()
-app.listen(80);
-
-var maxPlayers = 3;
-var index = 0;
-var fullOfPlayers = -1;
-
-var playerArray= [];
-                        
-//playerArray[player1.id]=(player1);
-//playerArray[player2.id]=(player2);
-//playerArray[player3.id]=(player3);
-
-var serverStage=new StageObject();
-var stageObject;
-var serverSideClientData;
-
-var indexPage = '/index.html'
-var noSlot = '/noSlot.html'
-var client = '/pacman-cliente.html'
-var gamePage = '/pacman-cliente.html'
-
-app.get('/', function (req, res) {
-	console.log("Enter");
-  if(index > maxPlayers){
-	res.sendfile(__dirname + noSlot);
-  }else { 
-	res.sendfile(__dirname + gamePage);
-
-  }
-});
-
-app.get('/client', function (req, res) {
-  res.sendfile(__dirname + client);
- });
- 
-app.io.sockets.on('connection', function (socket) {
-  console.log('Someone has connected \n');
-  console.log('Number of players: '+index+ '\n');
-  socket.on('my other event', function (data) {
-    console.log(data);
-	socket.broadcast.emit('doSomething');
-  });
-  
-  socket.on('requestID',function (data) {
-	console.log('server - request ID received');
-    if(index < maxPlayers){
-		var player= new Player(0,0,index);
-		playerArray[player.id] = (player);
-		socket.emit('requestedID',index);
-		console.log('Request ID required');
-		app.io.sockets.emit('sendDataToClient',sendDataToClient());
-		index++;
-	}else{
-		socket.emit('requestedID',fullOfPlayers);
-	}
-  });
-  
-  socket.on('receiveDataFromClient',function (unparsedJSON) {
-	
-	if(unparsedJSON != null){
-		parsedJSON = JSON.parse(unparsedJSON);
-		playerArray[parsedJSON.id].action = parsedJSON.action;
-        playerArray[parsedJSON.id] = serverStage.animatePlayer(playerArray[parsedJSON.id]);   
-	}
-	
-	sockets.emit('sendDataToClient',sendDataToClient());
-  });
-});
-
-
-              
-function sendDataToClient(){
-    var objArray = new Array(serverStage,playerArray);
-    stageObject= JSON.stringify(objArray);
-console.log(stageObject);	
-	return stageObject;
-}
-              
-function animatePlayersInServer(){
-    var player;
-    for(var playerNum=0;playerNum<playerArray.length;playerNum++){
-        player=playerArray[playerNum];
-        serverStage.animatePlayer(player);
-         }
-        sendDataToClient();
-     }
-              
-function receiveDataFromClient(a){
-    console.log(a);
-    if(a!=null){    
-        serverSideClientData=JSON.parse(a);
-        playerArray[serverSideClientData.id].action=serverSideClientData.action;
-        playerArray[serverSideClientData.id]=serverStage.animatePlayer(playerArray[serverSideClientData.id]);   
-    }
-}
-
 //STAGE OBJECT -----------------------------------------------------------------------------------------------------------------
     function StageObject(){
         console.log("Game Object Created");
@@ -213,3 +115,113 @@ function receiveDataFromClient(a){
         return color;  
     }
 //END STAGE OBJECT -------------------------------------------------------------------------------
+var app = require('express.io')()
+app.http().io()
+app.listen(80);
+
+var maxPlayers = 3;
+var index = 0;
+var fullOfPlayers = -1;
+
+var playerArray= [];
+                        
+//playerArray[player1.id]=(player1);
+//playerArray[player2.id]=(player2);
+//playerArray[player3.id]=(player3);
+
+var serverStage=new StageObject();
+serverStage.createStage();
+var serverSideClientData;
+var indexPage = '/index.html'
+var noSlot = '/noSlot.html'
+var client = '/pacman-cliente.html'
+var gamePage = '/pacman-cliente.html'
+
+app.get('/', function (req, res) {
+	console.log("Enter");
+  if(index > maxPlayers){
+	res.sendfile(__dirname + noSlot);
+  }else { 
+	res.sendfile(__dirname + gamePage);
+
+  }
+});
+
+app.get('/client', function (req, res) {
+  res.sendfile(__dirname + client);
+ });
+ 
+app.io.sockets.on('connection', function (socket) {
+  console.log('Someone has connected \n');
+  console.log('Number of players: '+index+ '\n');
+  socket.on('my other event', function (data) {
+    console.log(data);
+	socket.broadcast.emit('doSomething');
+  });
+  
+  socket.on('requestID',function (data) {
+	console.log('server - request ID received');
+    if(index < maxPlayers){
+		var player= new Player(0,0,index);
+		playerArray[player.id] = (player);
+		socket.emit('requestedID',index);
+		console.log('Request ID required');
+		app.io.sockets.emit('sendDataToClient',sendDataToClient());
+		index++;
+	}else{
+		socket.emit('requestedID',fullOfPlayers);
+	}
+  });
+  
+  socket.on('receiveDataFromClient',function (unparsedJSON) {
+	
+	if(unparsedJSON != null){
+		parsedJSON = JSON.parse(unparsedJSON);
+		playerArray[parsedJSON.id].action = parsedJSON.action;
+          
+        playerArray[parsedJSON.id] = serverStage.animatePlayer(playerArray[parsedJSON.id]);   
+	}
+	
+	app.io.sockets.emit('sendDataToClient',sendDataToClient());
+  });
+});
+
+setInterval(animatePlayer,90);
+
+function animatePlayer(){
+          if(playerArray!=null){
+                    for(var numPlayer=0;numPlayer<playerArray.length;numPlayer++){
+                          if(playerArray[numPlayer]!=null){
+                              serverStage.animatePlayer(playerArray[numPlayer]);
+                          }
+                    }
+                    app.io.sockets.emit('sendDataToClient',sendDataToClient());
+          }         
+}
+              
+function sendDataToClient(){
+    var objArray = new Array(serverStage,playerArray);
+    var data= JSON.stringify(objArray);
+    data= JSON.parse(data);
+	return data;
+}
+              
+function animatePlayersInServer(){
+    var player;
+    for(var playerNum=0;playerNum<playerArray.length;playerNum++){
+        player=playerArray[playerNum];
+        serverStage.animatePlayer(player);
+         }
+        sendDataToClient();
+     }
+              
+function receiveDataFromClient(a){
+    console.log(a);
+    if(a!=null){    
+        serverSideClientData=JSON.parse(a);
+        playerArray[serverSideClientData.id].action=serverSideClientData.action;
+        playerArray[serverSideClientData.id]=serverStage.animatePlayer(playerArray[serverSideClientData.id]);   
+    }
+}
+
+
